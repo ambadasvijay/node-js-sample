4e29f80355e30dafc87f2a2e496e2e3aed238105
Ajay George <ajaygeorge91@gmail.com>
2018-11-19 16:59:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var myArray = require('./myArray.json');
 var express = require('express')
 var app = express()
 
@@ -5,8 +6,18 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+	let pan = request.query.pan;
+	response.send(search(pan))
 })
+
+function search(pan){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].PAN === pan) {
+            return myArray[i];
+        }
+    }
+    return "NOT FOUND"
+}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
