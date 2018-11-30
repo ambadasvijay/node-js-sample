1acb344486ad5b70fbf816d4f372b5b9d49c3a69
mihir-saudagar <40380097+mihir-saudagar@users.noreply.github.com>
2018-11-30 14:41:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,45 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
 	let pan = request.query.pan;
-	response.send(search(pan));
+	let age =  request.query.age;
+	if(pan && age){
+		response.send(searchByPanAndAge(pan,age));
+	}else if (pan){
+		response.send(search(pan));
+	}else if (age){
+		response.send(searchByAge(age));
+	}else{
+		response.send("send pan or age or both");
+	}
+})
+
+// get by age (first match)
+app.get('/age/', function(request, response) {
+	let pan = request.query.age;
+	response.send(searchByAge(age));
 })
 
 function search(pan){
     for (var i=0; i < myArray.length; i++) {
         if (myArray[i].PAN === pan) {
+            return myArray[i];
+        }
+    }
+    return "NOT FOUND";
+}
+
+function searchByAge(age){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].Age === age) {
+            return myArray[i];
+        }
+    }
+    return "NOT FOUND";
+}
+
+function searchByPanAndAge(pan,age){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].Age === age && myArray[i].PAN === pan) {
             return myArray[i];
         }
     }
